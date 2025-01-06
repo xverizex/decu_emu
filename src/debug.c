13431e4f342ecc9cba99fd2b790d6b987fcd75b9
Dmitry <switchnight@mail.ru>
2025-01-06 11:15:19 +0500

    add colored offset from debug

--- src/debug.c
+++ src/debug.c
@@ -1,12 +1,25 @@
 #include "debug.h"
 #include <ncurses.h>
 
+struct debugger debugger = {14, 4};
+
 uint32_t
 is_debug_on_line (uint16_t line, uint8_t *posx, uint8_t *count)
 {
-	if (line == 0) {
-		*posx = 2;
-		*count = 4;
+	uint16_t start = debugger.offset / 16;
+	uint16_t end = (debugger.offset + debugger.count) / 16;
+
+	if (line >= start && line <= end) {
+		if (start != end) {
+			uint16_t d_count = 16 - debugger.offset;
+			*posx = ((line == start)? debugger.offset: 0);
+
+			*count = d_count;
+		} else {
+			*posx = debugger.offset;
+			*count = debugger.count;
+		}
+		return 0; // THIS SHOULD BE TRUE; IT JUST TESTING FOR FUTURE.
 	}
 
 	return 0;
--- src/debug.h
+++ src/debug.h
@@ -6,6 +6,11 @@
 #define COLOR_COMMON           1
 #define COLOR_STEP_DEBUG       2
 
+struct debugger {
+	uint16_t offset;
+	uint8_t count;
+};
+
 uint32_t is_debug_on_line (uint16_t line, uint8_t *posx, uint8_t *count);
 
 #endif
